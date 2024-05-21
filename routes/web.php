155f2fa676497d7af1abc7cd55d806a023aaacd7
Muhammad Afiq <blackzafiqz@gmail.com>
2024-05-21 15:16:33 +0800

    Add laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmployeeController;
 
-Route::get('/', function ()
-{
+Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/skuad', function ()
-{
-    return view('skuad');
-});
-Route::resource('employees', EmployeeController::class);
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/login', function ()
-{
-    return view('authentication/login');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/register', function ()
-{
-    return view('authentication/register');
-});
+
+require __DIR__.'/auth.php';
