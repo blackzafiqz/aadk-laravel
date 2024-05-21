e2d3e542ecb8aa5741efc617883919ed31d34e1a
Muhammad Afiq <blackzafiqz@gmail.com>
2024-05-21 15:41:45 +0800

    Add alamat model

---
+++ database/migrations/2024_05_21_071637_create_alamats_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alamat', function (Blueprint $table) {
+            $table->id();
+            $table->string('negeri')->nullable(false);
+            $table->string('daerah')->nullable(false);
+            $table->string('mukim')->nullable(false);
+            $table->string('poskod')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alamat');
+    }
+};
